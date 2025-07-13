1132d426d6146b60ee601b77a0540b769665b58f
Fovir <fovir@disroot.org>
2025-07-13 18:15:25 +0800

    feat: Add hint both entering and exiting the shell.

--- shell.nix
+++ shell.nix
@@ -8,10 +8,13 @@ pkgs.mkShell {
 
   shellHook = ''
     export NODE_ENV=development
+    echo "Entering the development environment!"
     echo "Node: $(node -v), pnpm: $(pnpm -v)"
 
     alias "dev"="pnpm run dev";
     alias "build"="pnpm run build";
     alias "clean"="pnpm run clean";
+
+    trap 'echo "Leaving the development environment!"' EXIT
   '';
 }

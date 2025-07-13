f1e90ea084d9535831b55a6b220b6683ab0c341e
Fovir <fovir@disroot.org>
2025-07-13 17:42:30 +0800

    feat: Update the `.gitignore`.

--- src/.vuepress/dist/assets/classDiagram-v2-PRA2ZCF7-BbCVwcUV.js
+++
@@ -1,1 +0,0 @@
-import{w as a,Y as s,k as t,u as e}from"./chunk-SHLHLJRN-BlG425Oq.js";import"./chunk-IO347I67-B16mKbz6.js";import{m as o}from"./mermaid.esm.min-B8yD-eE0.js";import"./app-BGTZnLUV.js";var p={parser:t,get db(){return new e},renderer:s,styles:a,init:o(r=>{r.class||(r.class={}),r.class.arrowMarkerAbsolute=r.arrowMarkerAbsolute},"init")};export{p as diagram};

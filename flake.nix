ff20c25078526dbd6234adb2ac1f7b61285a4fe5
Fovir <fovir@disroot.org>
2025-07-17 15:12:31 +0800

    fix: Fix the configuration of `flake`.

--- flake.nix
+++ flake.nix
@@ -5,22 +5,26 @@
 
   outputs = {
     self,
-    pkgs,
-  }: {
-    devShells.default = pkgs.lib.mkShell {
-      pkgs = import pkgs {system = "x86_64-linux";};
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
       buildInputs = with pkgs; [
         nodejs_20
         pnpm
       ];
+
       shellHook = ''
         export NODE_ENV=development
+
         echo "Entering the development environment!"
         echo "Node: $(node -v), pnpm: $(pnpm -v)"
 
-        alias "dev"="pnpm run dev"
-        alias "build"="pnpm run build"
-        alias "clean"="pnpm run clean"
+        alias "dev"="pnpm run dev";
+        alias "build"="pnpm run build";
+        alias "clean"="pnpm run clean";
 
         trap 'echo "Leaving the development environment!"' EXIT
       '';
